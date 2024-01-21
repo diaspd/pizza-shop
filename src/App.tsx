d9852b0c09b26bab3f0d1469f809b1a389a54ab4
Pedro Dias <pdr050203@gmail.com>
2024-01-21 13:53:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './global.css'
+
+import { Button } from './components/ui/button'
+
+export function App() {
+ return (
+  <div className='flex justify-center items-center h-screen'>
+    <Button>Enviar</Button>
+  </div>
+  )
+}
