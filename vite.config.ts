1095c4c97fd55da28f506708c2b5832414c3be6c
Pedro Dias <pdr050203@gmail.com>
2024-01-21 16:46:26 -0300

    add: routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import path from "node:path"
+import path from 'node:path'
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
