7be9a45c7d1cbb09ff6743524b8b9c72c87853d3
Pedro Dias <pdr050203@gmail.com>
2024-01-22 21:04:40 -0300

    add: sign-up page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -32,12 +33,14 @@ export function SignIn() {
           onClick: () => handleSignIn(data),
         },
         actionButtonStyle: {
-          backgroundColor: '#008A2E',
+          backgroundColor: 'transparent',
           fontWeight: 'bold',
+          color: 'green',
+          border: '2px solid #C1C1C3',
         },
       })
     } catch {
-      toast.error('Enviamos um link de autenticação para seu e-mail.')
+      toast.error('Credenciais inválidas.')
     }
   }
 
@@ -45,6 +48,10 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,9 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
   },
 ])
