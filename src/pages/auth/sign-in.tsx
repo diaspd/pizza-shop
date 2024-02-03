932dc638665d5c49f0473f16e32d0c8d3cd8a233
Pedro Dias <pdr050203@gmail.com>
2024-02-02 21:16:47 -0300

    add: user authentication

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,4 @@
+import { useMutation } from '@tanstack/react-query'
 import { motion } from 'framer-motion'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
@@ -5,6 +6,7 @@ import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -22,9 +24,13 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInForm) {
     try {
-      console.log(data)
+      await authenticate({ email: data.email })
 
       await new Promise((resolve) => setTimeout(resolve, 2000))
 
