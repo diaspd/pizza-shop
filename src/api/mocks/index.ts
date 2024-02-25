7623556901ec000a148658f1d6af0e14ac10cb4e
Pedro Dias <pdr050203@gmail.com>
2024-02-24 22:14:01 -0300

    add: dashboard mocks

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,9 +2,25 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
+import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getPopularProductsMock } from './get-popular-products-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
