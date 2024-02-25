86558e93b4e464d37a1d1df60442638f6c9b08c4
Pedro Dias <pdr050203@gmail.com>
2024-02-24 22:28:01 -0300

    renamed main

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,8 @@ import ReactDOMClient from 'react-dom/client'
 import { enableMSW } from './api/mocks/index.ts'
 import { App } from './app.tsx'
 
-const root = document.getElementById('root') as Element
-
 enableMSW().then(() => {
-  ReactDOMClient.createRoot(root).render(
+  ReactDOMClient.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
